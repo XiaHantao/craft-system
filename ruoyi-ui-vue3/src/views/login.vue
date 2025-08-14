fae94a8beff908c1964d13327efbfd9c5f78967e
Kanna Bush <lk644606288@163.com>
2025-08-14 15:21:15 +0800

    0814 系统图标、背景修改

--- ruoyi-ui-vue3/src/views/login.vue
+++ ruoyi-ui-vue3/src/views/login.vue
@@ -59,7 +59,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.</span>
+<!--      <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.</span>-->
     </div>
   </div>
 </template>
@@ -156,7 +156,7 @@ getCookie();
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
+  background-image: url("../assets/images/background.jpg");
   background-size: cover;
 }
 .title {
